b764a777211d76f79e1a00fdadba6f9b6146a791
UTNAK <29029659+UTNAK@users.noreply.github.com>
2024-05-24 12:51:50 -0700

    fix libstdc++.so6 has been commented out for debugging

--- src/analysis/SetUpEnvironment.sh
+++ src/analysis/SetUpEnvironment.sh
@@ -28,5 +28,6 @@ sudo apt-get update
 sudo apt-get install -y libglpk40
 
 # Fix libstdc++.so.6 for Quarto + Reticulate
-cd /opt/conda/envs/py39/lib/
-sudo cp libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+## comment out for debugging
+##cd /opt/conda/envs/py39/lib/
+##sudo cp libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
