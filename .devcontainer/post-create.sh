4fdac005cab2e0f53a2d18c09206129703eb5b39
UTNAK <29029659+UTNAK@users.noreply.github.com>
2024-05-30 20:58:49 +0000

    add liblpk install and libstdc fix

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -24,6 +24,16 @@ R -e "pak::pkg_install('UTNAK/tansakusuR')"
 R -e "pak::pkg_install('UTNAK/omlhashiR')"
 
 
+## Install libglpk40 package in the terminal for igraph
+sudo apt-get update
+sudo apt install -y libglpk40
+
+
+## Fix libstdc++.so.6 for Quarto + Reticulate
+cd /opt/conda/envs/py39/lib/
+sudo cp libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+
+
 # Run Gradle Task
 ./gradlew build
 ./gradlew owlLoad
