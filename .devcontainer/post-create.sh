b7cd4898dfeac8e704ca1d14ad1000e3639f59fa
UTNAK <29029659+UTNAK@users.noreply.github.com>
2024-05-31 00:02:49 +0000

    change where ./gradlew is executed

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -25,6 +25,11 @@ R -e "pak::pkg_install('UTNAK/tansakusuR')"
 R -e "pak::pkg_install('UTNAK/omlhashiR')"
 
 
+# Run Gradle Task
+./gradlew build
+./gradlew owlLoad
+
+
 ## Install libglpk40 package in the terminal for igraph
 sudo apt-get update
 sudo apt install -y libglpk40
@@ -35,6 +40,3 @@ cd /opt/conda/envs/py39/lib/
 sudo cp libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 
 
-# Run Gradle Task
-./gradlew build
-./gradlew owlLoad
